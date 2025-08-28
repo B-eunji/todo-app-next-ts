0d755a28ac1f278215596c1584ae70fd34907d45
B-eunji <a369pej@gmail.com>
2025-08-28 22:41:09 +0900

    feat: 수정

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,6 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        // 버킷명이 바뀔 여지가 있으면 아래처럼 와일드카드
         hostname: '**.s3.ap-northeast-2.amazonaws.com',
         pathname: '/**',
       },
